c4c2078d9ae1159759ce3bb697205cd6960889ee
Oli Kurt <oglimmer@gmail.com>
2013-01-20 23:45:56 +0100

    Added statistics

--- WEB/WAR_ROOT/deckbuilder.js
+++ WEB/WAR_ROOT/deckbuilder.js
@@ -1,7 +1,18 @@
-function Cards() {
+core_data.getBlock = function(blockNo) {
+	var retArray;
+	$.each([this.Dark,this.Light], function() {
+		//dark,light
+		$.each(this.CardBlocks, function() {
+			// array of {blockNo,cards}
+			if(this.blockNo == blockNo) {
+				retArray = this.cards;
+			}
+		});
+	});
+	return retArray;
+}
 
-	this.affilication = "none";
-	this.cardBlocks = [];
+function Cards() {
 
 	this.onlyOnce = [35,36,17,18]
 	this.affiliationRestrictions = [
@@ -9,8 +20,30 @@ function Cards() {
 		{affi:'Imperial Navy',blockNo:28},
 		{affi:'Jedi',blockNo:4},
 		{affi:'Rebel Alliance',blockNo:13}
-	]
+	];
 
+	this.reset();
+}
+
+Cards.prototype.reset = function() {
+	this.affilication = "none";
+	this.cardBlocks = [];
+	this.resetStatistics();
+}
+
+Cards.prototype.resetStatistics = function() {
+	this.totalCost = 0;
+	this.totalForce = 0;
+	this.totalObjectiveResources = 0;
+	this.totalNonObjectiveResources = 0;
+	this.totalUnitDmgCapacity = 0;	
+	this.totalObjectiveResources = 0;
+	this.totalAffiliationResources = this.affilication!='none'?1:0;
+	this.totalNumCards = 0;
+	this.totalCostCards = 0;
+	this.totalUnitCards = 0;
+	this.typesCounter = {};
+	this.affiliatonCounter = {};
 }
 
 Cards.prototype.restrictSelections = function() {
@@ -30,6 +63,46 @@ Cards.prototype.restrictSelections = function() {
 	});
 }
 
+Cards.prototype.calcStatistics = function() {
+	this.resetStatistics();	
+	var self = this;
+	$.each(this.cardBlocks, function() {
+		var cardArray = core_data.getBlock(this);
+		$.each(cardArray, function() {
+			self.totalCost += parseInt(this.Cost);
+			self.totalForce += parseInt(this.Force);
+
+			if(this.Type == 'Objective') {
+				self.totalObjectiveResources += parseInt(this.Resources);
+			} else {
+				self.totalNonObjectiveResources += parseInt(this.Resources);
+			}
+
+			self.totalNumCards++;
+			if(this.Type != 'Fate' && this.Type != 'Objective') {
+				self.totalCostCards++;
+			}
+			if(this.Type == 'Unit') {
+				self.totalUnitDmgCapacity += parseInt(this["Damage Capacity"]);
+				self.totalUnitCards++;
+			}
+
+			if(this.Type != 'Objective') {
+				if(typeof(self.typesCounter[this.Type]) == 'undefined') {
+					self.typesCounter[this.Type] = 0;
+				}
+				self.typesCounter[this.Type]++;
+			}
+
+			if(typeof(self.affiliatonCounter[this.Affiliation]) == 'undefined') {
+				self.affiliatonCounter[this.Affiliation] = 0;
+			}
+			self.affiliatonCounter[this.Affiliation]++;			
+		})
+	});	
+	console.log(this.typesCounter);
+}
+
 Cards.prototype.updateSelectionModel = function() {
 	var self = this;
 	$("input[type=radio]").each(function() {
@@ -46,18 +119,39 @@ Cards.prototype.updateSelectionModel = function() {
 }
 
 Cards.prototype.updateUi = function() {
+	var self = this;
 	var buff = '/gen.groovy?affi='+this.affilication+'&cards='+this.cardBlocks.toString();
 	if(this.affilication!='none'&&this.cardBlocks.length>=10) {
 		$("#out").html("<a href='"+buff+"'>Download your deck</a>"); 
 	} else {
 		$("#out").html("Deck not ready"); 
 	}
-	$('#selected').html("Sets selected:"+this.cardBlocks.length);	
+	var typesStr = "";
+	$.each(this.typesCounter, function(key, value) {
+		if(typesStr.length>0) typesStr += ", ";
+		typesStr += key.substring(0,2)+"="+value+" ("+Math.round(100*value/(self.cardBlocks.length*5))+"%)";
+	})
+	var affiliationStr = "";
+	$.each(this.affiliatonCounter, function(key, value) {
+		if(affiliationStr.length>0) affiliationStr += ", ";
+		affiliationStr += key.substring(0,2)+"="+value+" ("+Math.round(100*value/(self.cardBlocks.length*6))+"%)";
+	})
+	$('#selected').html("Sets selected: "+this.cardBlocks.length+"<br/>"+
+						"Cost: "+this.totalCost+" (avg:"+(this.totalCost/this.totalCostCards).toFixed(2)+")<br/>"+
+						"Force: "+this.totalForce+" (avg:"+(this.totalForce/(this.cardBlocks.length*5)).toFixed(2)+")<br/>"+
+						"Obj Resources: "+(this.totalAffiliationResources+this.totalObjectiveResources)+" (avg:"+(this.totalObjectiveResources/this.cardBlocks.length).toFixed(2)+")<br/>"+
+						"Oth Resources: "+this.totalNonObjectiveResources+" (avg:"+(this.totalNonObjectiveResources/(this.cardBlocks.length*5)).toFixed(2)+")<br/>"+
+						"Unit Dmg Capa: "+this.totalUnitDmgCapacity+" (avg:"+(this.totalUnitDmgCapacity/this.totalUnitCards).toFixed(2)+")<br/>"+
+						"Cards: "+this.totalNumCards+"<br/>"+
+						"Types: "+typesStr+"<br/>"+
+						"Affiliation: "+affiliationStr+"<br/>"
+						);	
 }
 
 Cards.prototype.selectionChanged = function() {
 	this.updateSelectionModel();
 	this.restrictSelections();
+	this.calcStatistics();
 	this.updateUi();
 }
 
@@ -65,8 +159,7 @@ Cards.prototype.createSide = function (side) {
 	$('#mainLink'+side).hide();
 	$('#mainLink'+(side=='Dark'?'Light':'Dark')).show();
 	$('#main').empty()
-	this.affilication = "none";
-	this.cardBlocks = [];
+	this.reset();
 	this.updateUi();
 
 	// create Affiliations-Box
